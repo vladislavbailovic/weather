73acc5bbd4daceb31326c6e277db5a413a0fe01b
Vladislav <vladislavbailovic@gmail.com>
2017-05-25 22:05:53 +0200

    Introduce initial parser implementation

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
 const data = require('./lib/data'),
-	service = require('./lib/service')
+	service = require('./lib/service'),
+	parse = require('./lib/parser')
 ;
 
 data.get(service(), raw => {
-	let data = JSON.parse(raw || '{}'),
-		list = (data || {}).list || []
-	;
-	if (!list.length) return false;
-
-	list.forEach(item => {
-		let desc = item.weather[0].description,
-			temp = item.main.temp,
-			tstamp = item.dt_txt
-		;
-		console.log(tstamp, temp,desc);
-	});
+	let list = parse(raw);
 });
