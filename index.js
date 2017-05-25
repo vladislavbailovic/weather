81cbf7c0d179bb16ce98268808146032731662fa
Vladislav <vladislavbailovic@gmail.com>
2017-05-25 21:38:34 +0200

    Refactoring, round one

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-const http = require('http'),
-	cfg = require('dotenv').config()
-;
+const data = require('./lib/data');
 
-const APIKEY = process.env.APIKEY,
-	SERVICE = 'http://api.openweathermap.org/data/2.5/forecast',
-	CITY = 'zrenjanin',
-	COUNTRY = 'sr'
-;
-
-const service = (city, country) => {
-	city = city || CITY;
-	country = country || COUNTRY;
-	return SERVICE + '?q=' + city + ',' + country + '&mode=json&units=metric&APPID=' + APIKEY;
-};
-
-const request = (cback) => {
-	http.get(service(), function (rsp) {
-		let raw = '';
-		rsp.on('data', (str) => { raw += str; });
-		rsp.on('end', () => { cback(raw); });
-	});
-};
-
-request(raw => {
+data.get(raw => {
 	let data = JSON.parse(raw || '{}'),
 		list = (data || {}).list || []
 	;
